caa972942fec3f79418e5f0a857a32bc47fad553
Allan Silva <allandasilva33@outlook.com>
2024-11-21 02:12:57 -0300

    Estilização do ícone do menu-hamburger

--- js/script.js
+++ js/script.js
@@ -1,9 +1,11 @@
-const ICON_MENU = document.querySelector('.icon-menu')
-
-ICON_MENU.addEventListener('click', menuOpen)
+const ICON_MENU = document.querySelector('.icon-menu');
 
 function menuOpen(){
-    const MENU_CONTENT = document.querySelector('.menu-content')
+    const MENU_CONTENT = document.querySelector('.menu-content');
+    const MENU_HAMBURGER = document.querySelector('.menu-hamburger');
 
-    MENU_CONTENT.classList.toggle('menu-content-active')
+    MENU_CONTENT.classList.toggle('menu-content-active');
+    MENU_HAMBURGER.classList.toggle('menu-hamburger-active');
 }
+
+ICON_MENU.addEventListener('click', menuOpen);
